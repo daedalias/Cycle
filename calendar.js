dd79da4169fff73b0b69c0852ad9ddc330e51c35
daedalias <pyrsonists@gmail.com>
2026-09-22 23:17:17 +0000

    v9.51 - more fix

--- calendar.js
+++ calendar.js
@@ -1,11 +1,19 @@
+let currentMonth = new Date();
+
 export function renderCalendar() {
     const calendar = document.getElementById("calendar");
 
     calendar.innerHTML = `
-        <button class="day">
-            TEST
+        <button id="testButton">
+            Click Me
         </button>
     `;
+
+    document
+        .getElementById("testButton")
+        .addEventListener("click", () => {
+            alert("clicked");
+        });
 }
 
 export function previousMonth() {}
